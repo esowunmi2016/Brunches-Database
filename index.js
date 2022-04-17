574de90024faf475c8caa72e85b8a6e569d7a679
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-17 22:40:19 +0100

    port update

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function(req, res){
     res.write('Holla mami you dey look good if you want to ride i go like to sample you')
     res.end()
-}).listen('8080');
+}).listen(process.env.PORT || 8080);
