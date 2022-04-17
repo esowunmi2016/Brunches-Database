a3d909f0eded9f5ce5c178cbc8903963a5305350
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-17 23:37:28 +0100

    mySql connection test

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ var http = require('http');
 
 
 var con = mysql.createConnection({
-  host: "showemz",
-  user: "root",
-  password: "Arike1Ayo"
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
 });
 
 
 http.createServer(function(req, res){
-    res.write('Holla mami you dey look good if you want to ride i go like to sample you')
+    res.write('testing 1 2')
     res.end()
 }).listen(process.env.PORT || 8080);
