534d898962a187f04f8f8f5cc90ce5c9ea658ea2
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 15:44:32 +0100

    .

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ client.connect();
 
 // const sql = 'SELECT table_schema,table_name FROM information_schema.tables;'
 // const sql = 'SELECT DB_NAME() AS Current Database;'
-const sql = `CREATE TABLE test (
-  id INT NOT NULL AUTO_INCREMENT,  
-  state VARCHAR(100) NOT NULL,  
-  rate REAL,
-  PRIMARY KEY(id)
-);`
+const sql = 'CREATE TABLE example_table ( a INTEGER );'
 
 client.query(sql, (err, res) => {
   if (err) throw err;
