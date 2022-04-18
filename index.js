14a7c9ee45d311523905fb7564639f45955be5f6
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 14:27:29 +0100

    bug fix

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { Client } = require('pg');
 function apiResponse(x){
 
   http.createServer(function(req, res){
+    for(i in x){
       res.write(x)
+    }
       res.end()
   }).listen(process.env.PORT || 8080);
 }
@@ -30,8 +32,8 @@ function checkTables(callBack){
     if (err) throw err;
     for (let row of res.rows) {
       // console.log(JSON.stringify(row));
-      // x.push(JSON.stringify(row))
-      callBack(JSON.stringify(row))
+      x.push(JSON.stringify(row))
+      // callBack(JSON.stringify(row))
     }
     client.end();
   });
