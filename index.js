19d885d16c2c09fd22cc365d01bf403d362ca69e
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 22:07:26 +0100

    ,

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.post('/', (req, res) => {
 
 
   const sql = `
-  INSERT INTO customerbase (firstname)
-  VALUES(${fname} );
+  INSERT INTO customerbase (firstname, lastname, email, phonenumber)
+  VALUES('${fname}', '${lname}','${email}','${number}',);
   `
 
   client.query(sql, (err, res) => {
