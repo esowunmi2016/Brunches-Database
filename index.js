80d46a657717ed0861563ca39d5542a4b195822d
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 14:20:13 +0100

    postgres tables check function implementation

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ var http = require('http');
 const { Client } = require('pg');
 
 
-http.createServer(function(req, res){
-    res.write('testing 1 2')
-    res.end()
-}).listen(process.env.PORT || 8080);
+function apiResponse(x){
+
+  http.createServer(function(req, res){
+      res.write(x)
+      res.end()
+  }).listen(process.env.PORT || 8080);
+}
 
 
+function checkTables(callBack){
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
-client.connect();
+  var x = []
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect();
+  
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      // console.log(JSON.stringify(row));
+      x.push(JSON.stringify(row))
+    }
+    client.end();
+  });
+
+  callBack(x)
+
+}
+
+checkTables(apiResponse)
+
