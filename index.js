9c1b340c89e498695fe24b0c93754c454d3d3368
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 19:26:01 +0100

    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
 
 
