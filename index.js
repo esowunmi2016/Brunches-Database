75827063e62d85b35644a42aaae3ec43e61bad0e
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 15:57:02 +0100

    .

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ client.connect();
 // const sql = 'SELECT table_schema,table_name FROM information_schema.tables;'
 // const sql = 'SELECT DB_NAME() AS Current Database;'
 
-const sql = 'DROP TABLE [IF EXISTS] example_table [CASCADE | RESTRICT]; '
+// const sql = 'DROP TABLE [IF EXISTS] example_table [CASCADE | RESTRICT]; '
 
-// const sql = 'CREATE TABLE example_table ( id INTEGER );'
+
+const sql = `CREATE TABLE example_table ( 
+  id SERIAL
+   );`
 
 client.query(sql, (err, res) => {
   if (err) throw err;
