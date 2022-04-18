b3ae4139de554fac4bbc47440269c01178d3bad1
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 19:16:41 +0100

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 //     res.end()
 // }).listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 8080, function(){
+var server = app.listen(process.env.PORT || 8080, function(){
   var host = server.address().address  
   var port = server.address().port  
   console.log("Example app listening at http://%s:%s", host, port)  
