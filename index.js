2a2ec1e3e4b3e569b9a55d0954aa3e604f9c0a3c
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-18 14:01:45 +0100

    postgres installation

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 var mysql = require('mysql');
 var http = require('http');
+const { Client } = require('pg');
 
 
 http.createServer(function(req, res){
     res.write('testing 1 2')
     res.end()
 }).listen(process.env.PORT || 8080);
+
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
