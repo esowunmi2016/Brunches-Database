e7f008ca0d31deb854e0edceebda48954462fb9d
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-19 14:44:35 +0100

    .

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { Client } = require('pg');
-
+var cors = require('cors')
 const express = require('express');
 const app = express()
+app.use(cors()) 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
