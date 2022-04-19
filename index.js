d2531fe6fa05352dec4955a52c61b4306ec50b00
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-19 10:10:13 +0100

    .

--- index.js
+++ index.js
@@ -16,21 +16,11 @@ const client = new Client({
 
 client.connect();
 
-
 app.post('/', (req, res) => {
-
   const fname = req.body.fname;
   const lname = req.body.lname;
   const email = req.body.email;
   const number = req.body.number;
-
-  // res.send({
-  //   "First name":fname,
-  //   "Last name":lname,
-  //   "Email":email,
-  //   "Phone number":number,
-  // })
-
 
   const sql = `
   INSERT INTO customerbase (firstname, lastname, email, phonenumber)
@@ -44,38 +34,7 @@ app.post('/', (req, res) => {
     console.log(res)
     client.end();
   });
-
   res.send('successful')
-
 })
 
 app.listen(process.env.PORT || 8080)
-
-
-// const sql = 'SELECT table_schema,table_name FROM information_schema.tables;'
-// const sql = 'SELECT DB_NAME() AS Current Database;'
-
-// const sql = 'DROP TABLE IF EXISTS example_table; '
-
-
-// USED TO CREATE THE TABLE
-
-// const sql = `CREATE TABLE customerbase (
-// 	id serial PRIMARY KEY,
-// 	firstname VARCHAR ( 50 ) NOT NULL,
-// 	lastname VARCHAR ( 50 ) NOT NULL,
-// 	email VARCHAR ( 255 ) NOT NULL,
-//   phoneNumber INT NOT NULL
-// );`
-
-
-
-
-// client.query(sql, (err, res) => {
-//   if (err) throw err;
-//   // for (let row of res.rows) {
-//   //   console.log(JSON.stringify(row));
-//   // }
-//   console.log(res)
-//   client.end();
-// });
