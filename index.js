8b5f01c8d1c0646b58de790a0264f1259beecfec
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-28 09:22:57 +0100

    .

--- index.js
+++ index.js
@@ -42,12 +42,14 @@ app.post('/', (req, res) => {
 });
 
 app.get('/', (req, res)=>{
-  // console.log('current page is ' + req.query.page)
+  // console.log('current page is ' + req.query.page) 
   const pageNumber = req.query.page
   const pageSize = 20
   const sql = `
     SELECT * 
     FROM brunchesCustomerbase 
+    OFFSET ${pageNumber*pageSize}
+    FETCH FIRST ${pageSize} ROW ONLY;
   `
   client.query(sql, (err, response)=>{
     if (err){
