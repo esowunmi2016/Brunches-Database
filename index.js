7d883cba674972e52967bddf20f02c6e9330471e
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-29 04:11:14 +0100

    .

--- index.js
+++ index.js
@@ -55,9 +55,6 @@ app.get('/', (req, res)=>{
     SELECT * 
     FROM brunchesCustomerbase 
     WHERE UPPER(firstname) LIKE UPPER('${fname}')
-    or UPPER(lastname) LIKE UPPER('${lname}')
-    or UPPER(email) LIKE UPPER('${email}')
-    or UPPER(phonenumber) LIKE UPPER('${number}')
     OFFSET ${pageNumber*pageSize-pageSize} ROWS
     FETCH FIRST ${pageSize} ROW ONLY 
   `
