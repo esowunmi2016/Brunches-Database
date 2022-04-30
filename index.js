e085e8adf8c75d54098d84054d615620d0e056ed
esowunmi2016 <esowunmi2016@fau.edu>
2022-04-30 14:36:33 +0100

    .

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/', (req, res)=>{
     res.send(response.rows)
     client.query(sql2, (err, response)=>{
       console.log('3 '+response)
-      console.log('4 '+response.count)
+      // console.log('4 '+response.count)
       // res.send(response.rows)
     })
   })
